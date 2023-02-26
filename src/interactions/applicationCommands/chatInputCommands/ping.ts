bdefd458905b1f1fa5bd8cc9c80653f3025b13f8
justCallMeJade <87892483+justCallMejade@users.noreply.github.com>
2023-02-26 14:31:15 +0800

    Initial Changes; Test Files

--- src/interactions/applicationCommands/chatInputCommands/ping.ts
+++ src/interactions/applicationCommands/chatInputCommands/ping.ts
@@ -1,12 +1,15 @@
-import { ChatInputCommandInteraction } from '../../classes/Interaction'
+import { ApplicationCommandType } from 'discord.js'
+import { ChatInputCommandInteraction } from '../../../classes/Interaction'
 
 export default new ChatInputCommandInteraction({
   name: 'ping',
+  type: ApplicationCommandType.ChatInput,
   description: 'Replies with pong!',
   category: 'Information',
   status: true,
   replyType: 'DeferReplyEphemeral',
   callbackHandler: async ({ interaction }) => {
+    await interaction.deferReply({ephemeral: true})
     await interaction.followUp({ content: 'Pong!' })
   }
 })
---
+++ src/interactions/applicationCommands/contextMenu/message/getContent.ts
@@ -0,0 +1,14 @@
+import { ApplicationCommandType } from "discord.js";
+import { MessageContextMenuCommandInteraction } from "../../../../classes/Interaction";
+
+export default new MessageContextMenuCommandInteraction({
+    name: `Get Content`,
+    type: ApplicationCommandType.Message,
+    status: {
+        enabled: true
+    },
+    callbackHandler: async ({ interaction }) => {
+        await interaction.deferReply({ephemeral: true})
+        await interaction.followUp({ content: `${interaction.targetMessage.content}` })
+    }
+})
